fa19211d3e9ea01b91d375dc8f4e08126c8f4f4a
Suppakorn <suwannapop01@gmail.com>
2023-12-17 00:23:48 +0700

    First Commit not finish yet just add methode that should have

---
+++ src/Set.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+public class Set <E>  {
+    Set<E> set ;
+    public Set() {
+        set = new Set<>();
+    }
+    public  boolean containsAll(){
+    }
+    public Set<E> addAll(Set<E> set){
+    }
+
+    public  Set<E> removeAll(Set<E> set){
+
+    }
+
+    public Set<E> retainAll (Set<E> set) {
+
+    }
+    public void voidadd(E input){
+
+    }
+}
